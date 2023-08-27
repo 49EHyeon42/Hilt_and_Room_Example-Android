4ce91f252f0b5ab8fde7e26b45f1ed5eeef26b62
TaeHyeon Jo <78364654+49EHyeon42@users.noreply.github.com>
2023-08-27 16:57:41 +0900

    fix: @NonNull annotation 제거, update method 1개 추가

--- app/src/main/java/dev/ehyeon/hiltandroomexampleapplication/data/memo/MemoDao.java
+++ app/src/main/java/dev/ehyeon/hiltandroomexampleapplication/data/memo/MemoDao.java
@@ -1,31 +1,34 @@
 package dev.ehyeon.hiltandroomexampleapplication.data.memo;
 
-import androidx.annotation.NonNull;
 import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.Query;
+import androidx.room.Update;
 
 @Dao
 public interface MemoDao {
 
     @Query("SELECT * FROM memo WHERE id = :id")
-    Memo selectMemoById(@NonNull Long id);
+    Memo selectMemoById(Long id);
 
     @Query("SELECT * FROM memo")
     Memo selectAllMemo();
 
     @Insert
-    void insertMemo(@NonNull Memo... memos);
+    void insertMemo(Memo... memos);
+
+    @Update
+    void updateMemo(Memo... memos);
 
     @Query("UPDATE memo SET title = :title, content = :content WHERE id = :id")
-    void updateMemoById(@NonNull Long id, @NonNull String title, @NonNull String content);
+    void updateMemoById(Long id, String title, String content);
 
     @Delete
-    void deleteMemo(@NonNull Memo memo);
+    void deleteMemo(Memo memo);
 
     @Query("DELETE FROM memo WHERE id = :id")
-    void deleteMemoById(@NonNull Long id);
+    void deleteMemoById(Long id);
 
     @Query("DELETE FROM memo")
     void deleteAllMemo();
